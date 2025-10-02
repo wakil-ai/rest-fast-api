9a54a05269de24d0c1f06f75e3def9177753dab5
m <abdurasulovm149@gmail.com>
2025-10-02 14:04:46 +0000

    feat: Add regex for sentence splitting in ChatChain and update uvicorn dependency in requirements

--- app/retrieval/retrieval_service.py
+++ app/retrieval/retrieval_service.py
@@ -3,7 +3,6 @@ from app.core.config import settings
 from app.core.logger import logger
 from app.db.db_manager import DBManager
 from app.retrieval.embedding_manager import EmbeddingManager
-from app.utils.text_cleaning import remove_braces
 import re
 
 
