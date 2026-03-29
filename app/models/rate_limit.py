1b8d5fbf31026a92bec57e7fee7f457c66cd458d
mirsaidl <abdurasulovm149@gmail.com>
2026-03-30 07:26:22 +0900

    refactor: Remove credit_costs field from RateLimitResponse model and related logic

--- app/models/rate_limit.py
+++ app/models/rate_limit.py
@@ -5,8 +5,6 @@ class RateLimitResponse(BaseModel):
     user_id: str
     remaining_credits: int
     daily_credit_limit: int
-    credit_costs: dict
-
 
 class ResetLimitRequest(BaseModel):
     user_id: str
