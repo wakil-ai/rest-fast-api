a7364f4774aa29452c07af0dcb46b7f4ccb8f6f1
mirsaidl <mirsaid.hbai@gmail.com>
2026-05-20 09:27:06 +0900

    refactor: simplify auth service imports by removing unused functions and adding data validation

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,8 +1,5 @@
 from app.services.bitrix24_service import Bitrix24Service
-from app.services.auth_service import (
-    exchange_telegram_oauth_code,
-    verify_telegram_id_token,
-)
+from app.services.auth_service import validate_telegram_data
 from app.services.chat_history_service import ChatHistoryService
 from app.services.chat_service import ChatService
 from app.services.file_management import FileManager
